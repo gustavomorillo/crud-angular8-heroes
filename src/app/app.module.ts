ba8879d8abc708225937111e5d91bb9c177f00f5
Gustavo Morillo <gustavomorillo@gmail.com>
2020-01-09 22:23:33 -0400

    first commit to deploy at Heroku

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,7 @@ import { AppRoutingModule } from './app-routing.module';
     HeroesComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     FormsModule,
     HttpClientModule
